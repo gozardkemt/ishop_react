c78193037e6930d5eeb0f12fb5e654465d98592e
Gorazd Kmeť <goro.gozy@gmail.com>
2020-02-02 13:46:15 +0100

    Merge

--- src/App.js
+++ src/App.js
@@ -1,77 +1,5 @@
 import React from 'react';
 import Header from './Header.js';
-<<<<<<< HEAD
-import './App.css';
-
-function App(props) {
-
-  return (
-    <>
-	  < Header />
-	  < Content categories={props.categories} products={props.products} />
-    </>
-  );
-}
-
-export default App;
-
-
-export function Options(props) {
-
-	return (
-		<>
-		{
-			props.categories.map( c => <option id={c.id} value={c.name.toLowerCase()}>{c.name}</option> )
-		}
-		</>
-	)
-}
-
-
-let activeCategoryId = '0';
-
-function changeActiveCategory(e) {
-	activeCategoryId = e.target.selectedOptions[0].id
-}
-
-export function Content(props) {
-
-	const contentStyle = {	display:'grid', gridTemplateColumns:'repeat(3, 1fr)' };
-	const { categories, products } = props;
-
-	return (
-		<>
-			<select onChange={changeActiveCategory} className='options'>
-			  <option id='0' value='all' defaultValue>Všetky</option>
-			  < Options categories={categories} />
-			</select>
-			<main id='content' style={contentStyle} >
-				< ProductItem activeCategoryId={activeCategoryId} categories={categories} products={products} />
-			</main>
-		</>
-	)
-}
-
-export function ProductItem(props) {
-
-	const { products, categories, activeCategoryId } = props;
-	const productStyle = { border:'10px solid rgba(90,90,90,0.3)', margin:'5px' };
-	let filteredProducts = products.filter(p => activeCategoryId === p.categoryId);
-	if (activeCategoryId === '0') { filteredProducts = products };
-
-	return (
-		<>
-			{
-				filteredProducts.map( p =>
-						<div key={p.src} className='product' style={productStyle}>
-							< ProductName key={p.name} name={p.name} />
-							< ProductPrice key={p.price} price={p.price} />
-							< ProductCategory key={p.src} categoryId={p.categoryId} categories={categories} />
-							< ProductImg key={p.src} src={p.thumbnail} />
-						</div>
-
-				)
-=======
 import Content from './Content.js';
 import FilterBar from './FilterBar.js';
 import ShoppingCard from './ShoppingCard.js';
@@ -175,7 +103,6 @@ export default class App extends React.Component {
 				return {
 					shoppingCard: state.shoppingCard
 				}
->>>>>>> classed
 			}
 		)
 	}
